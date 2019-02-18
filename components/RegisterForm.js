98748a63309ed30432b9074224d0da9cc0a0b1dd
Hieu.Tran <hieu.tran@HieuTrans-MacBook-Pro.local>
2019-02-18 20:49:44 +0700

    add validate phone

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -4,6 +4,45 @@ import {connect} from 'react-redux';
 import { Form, Field, reduxForm } from 'redux-form'
 import userActions from '../actions/userActions'
 
+const validatePhone = (strPhone) => {
+	const trimUnicode = (str) => {
+    // Chuyển hết sang chữ thường
+    str = str.trim();
+    str = str.toLowerCase();
+    // xóa dấu
+    str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+    str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+    str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+    str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+    str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+    str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+    str = str.replace(/(đ)/g, 'd');
+
+    // Xóa ký tự đặc biệt
+    str = str.replace(/([^0-9a-z-\s])/g, '');
+    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'| |\"|\&|\#|\[|\]|\-|~|$|_/g, '');
+
+    // Xóa khoảng trắng thay bằng ký tự -
+    str = str.replace(/(\s+)/g, '-');
+    //
+    str = str.replace(/-+-/g, '-'); // thay thế 2- thành 1-
+    // str = str.replace(/-/g, ''); // thay thế - thành ''
+
+    // xóa phần dự - ở đầu
+    str = str.replace(/^-+/g, '');
+
+    // xóa phần dư - ở cuối
+    str = str.replace(/-+$/g, '');
+
+    // return
+    return str;
+  };
+	const arrayPhone = ['09', '03', '07', '08', '05'];
+	const phoneFormat = trimUnicode(`${strPhone}`);
+	const strFirt = phoneFormat.slice(0,2);
+  return (arrayPhone.includes(strFirt) && (phoneFormat.length === 10)) ? true : false;
+};
+
 const RenderField = ({
 	input,
 	label,
@@ -26,9 +65,10 @@ const validate = values => {
 	if (!values.fullname) {
 		errors.fullname = 'Vui lòng nhập Họ Tên'
 	}
-
 	if (!values.phone) {
 		errors.phone = 'Vui lòng nhập Số Điện Thoại'
+	} else if (!validatePhone(values.phone || '')) {
+		errors.phone = 'Số Điện Thoại không hợp lệ'
 	}
 
 	if (!values.email) {
